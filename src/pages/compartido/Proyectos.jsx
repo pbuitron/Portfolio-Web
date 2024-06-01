16ffdbf20be1de8a13324285e525928c0bd15261
pbuitron <pbuitron@users.noreply.github.com>
2024-05-31 21:13:38 -0500

    portfolio al 95%

--- src/pages/compartido/Proyectos.jsx
+++ src/pages/compartido/Proyectos.jsx
@@ -10,24 +10,32 @@ const Proyectos = () => {
         { proyecto: "https://sealpro.vercel.app/", año: "2023", tt: t('projects.ecommerce') },
         { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
         { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
-        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') }
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.clinica') },
+        { proyecto: "https://sealpro.vercel.app/", año: "2022", tt: t('projects.sealpro') }
     ];
 
     return (
         <article className='proyectos'>
             <section className='proyecto'>
-            {proyectos.map((proyecto, index) => (
-                <section className='proyectos-contenedor'>
-                    <Link to={proyecto.proyecto}className='proyectos-link' target="_blank" rel="noopener noreferrer">
-                        <p className='proyectos-nombre'>{proyecto.tt}</p>
-                    </Link>
-                    <span className="proyectos-fecha" key={index}>
-                        {proyecto.año}
-                    </span>
-                    
-
-                </section>
-            ))}
+                {proyectos.map((proyecto, index) => (
+                    <section className='proyectos-contenedor' key={index}>
+                        <Link to={proyecto.proyecto} className='proyectos-link' target="_blank" rel="noopener noreferrer">
+                            <p className='proyectos-nombre'>{proyecto.tt}</p>
+                        </Link>
+                        <span className="proyectos-fecha">
+                            {proyecto.año}
+                        </span>
+                    </section>
+                ))}
             </section>
         </article>
     );
