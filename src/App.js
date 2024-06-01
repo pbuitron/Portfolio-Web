3425f4e179476a93f7fcfd7240f0918d2b87a0f2
pbuitron <pbuitron@users.noreply.github.com>
2024-06-01 10:49:47 -0500

    PRELOADER MODIFICADO - BOTON AGREGADO

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react';
+/*import React, {useState,useEffect} from 'react';
 import LightingEffect from './Components/LightingEffect/LightingEffect';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './Components/Header/Header';
@@ -41,6 +41,47 @@ function App() {
 
         </div>
 
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;*/
+
+import React, { useState } from 'react';
+import LightingEffect from './Components/LightingEffect/LightingEffect';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import './Styles/App.css';
+import './apply.css'
+import Principal from './pages/principal/Principal';
+import Fondo from './Components/Fondo/Fondo';
+import Preloader from './Components/Preloader/Preloader';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
+
+  
+
+  const handleFinishLoading = () => {
+    setFadeOut(true);
+    setTimeout(() => setLoading(false), 1000); // Tiempo para el fade out
+  };
+
+  if (loading) {
+    return <Preloader fadeOut={fadeOut} onFinishLoading={handleFinishLoading}  />;
+  }
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className='border'>
+          <Fondo />
+          <LightingEffect />
+          <Header />
+          <Principal />
+        </div>
       </div>
     </BrowserRouter>
   );
--- src/Components/Preloader/Preloader.jsx
+++ src/Components/Preloader/Preloader.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import './Preloader.css'
 const Preloader = () => {
     return (
@@ -13,6 +13,26 @@ const Preloader = () => {
             </div>
         </div>
     );
+};
+
+export default Preloader;*/
+import React from 'react';
+import './Preloader.css';
+
+const Preloader = ({ fadeOut, onFinishLoading }) => {
+  return (
+    <div className={`preloader ${fadeOut ? 'fade-out' : ''}`}>
+      <div className="preloader-wrapper">
+        <h1 className="glitch" data-text="Paul Buitron">
+          Paul Buitron
+        </h1>
+        <span className="sub">{`<Web_Developer/>`}</span>
+        <button className="preloader-button " onClick={onFinishLoading}>
+          START
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Preloader;
